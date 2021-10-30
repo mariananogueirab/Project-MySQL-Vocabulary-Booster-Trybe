3273a71f6ce03b6dbb1c038d1209127ebede1c4e
mariananbp <mariananbp@gmail.com>
2021-10-30 18:21:04 -0300

    Challenge 10 - codeclimate

--- desafio10.sql
+++ desafio10.sql
@@ -1,7 +1,4 @@
-SELECT p.ProductName AS `Produto`,
-	MIN(o.Quantity) AS `Mínima`,
-    MAX(o.Quantity) AS `Máxima`,
-	ROUND(AVG(o.Quantity), 2) AS `Média`
+SELECT p.ProductName AS `Produto`, MIN(o.Quantity) AS `Mínima`, MAX(o.Quantity) AS `Máxima`, ROUND(AVG(o.Quantity), 2) AS `Média`
 FROM w3schools.products AS p
 INNER JOIN w3schools.order_details AS o
 ON p.ProductID = o.ProductID
