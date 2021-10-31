e28e118aa7a2881b2079c455c8a2f626ed4cf1db
mariananbp <mariananbp@gmail.com>
2021-10-30 21:41:49 -0300

    Challenge 16

---
+++ desafio16.sql
@@ -0,0 +1,17 @@
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(100))
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE total_of_jobs INT;
+	SELECT COUNT(j.EMPLOYEE_ID)
+    FROM hr.job_history AS j
+    INNER JOIN hr.employees AS e
+    ON j.EMPLOYEE_ID = e.EMPLOYEE_ID
+    WHERE e.EMAIL = email
+    INTO total_of_jobs;
+    RETURN total_of_jobs;
+END $$
+DELIMITER ;
+
+SELECT buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
