5928c810522151a7c3d9a6ab4f81a984b00410cc
mariananbp <mariananbp@gmail.com>
2021-10-30 21:42:56 -0300

    Challenge 16 - code climate

--- desafio16.sql
+++ desafio16.sql
@@ -4,7 +4,7 @@ CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(100)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE total_of_jobs INT;
-	SELECT COUNT(j.EMPLOYEE_ID)
+SELECT COUNT(j.EMPLOYEE_ID)
     FROM hr.job_history AS j
     INNER JOIN hr.employees AS e
     ON j.EMPLOYEE_ID = e.EMPLOYEE_ID
